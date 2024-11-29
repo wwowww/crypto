9621056155f749b1d8e3f320ba9ff548df74a3f9
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-29 19:35:30 +0900

    style. 복잡한 이름에서 response로 수정

--- src/hooks/useMarketData.ts
+++ src/hooks/useMarketData.ts
@@ -9,40 +9,42 @@ export const useMarketData = ({id, currency, days}: Market) => {
   const { data: singleCoin } = useQuery({
     queryKey: ['singleCoin', id],
     queryFn: async () => {
-      const marketSingCoinData = await fetchMarketSingleCoinData(id);
-      setMarketData(marketSingCoinData); // Zustand 스토어 저장
+      if (!id) return null;
+      const response = await fetchMarketSingleCoinData(id);
+      setMarketData(response); // Zustand 스토어 저장
       
-      return marketSingCoinData; // ReactQuery 캐시에 저장
+      return response.data; // ReactQuery 캐시에 저장
     },
   });
 
   const { data: coinListAll } = useQuery({
     queryKey: ['coinListAll', currency],
     queryFn: async () => {
-      const marketCoinListAllData = await fetchMarketCoinListAllData(currency);
-      setMarketData(marketCoinListAllData);
-
-      return marketCoinListAllData;
+      if (!currency) return null;
+      const response = await fetchMarketCoinListAllData(currency);
+      return response.data;
     },
   })
 
   const { data: HistoricalChart } = useQuery({
     queryKey: ['HistoricalChart', currency],
     queryFn: async () => {
-      const marketHistoricalChartData = await fetchMarketHistoricalChartData(id, currency, days);
-      setMarketData(marketHistoricalChartData);
+      if (!id || !currency || !days) return null;
+      const response = await fetchMarketHistoricalChartData(id, currency, days);
+      setMarketData(response);
 
-      return marketHistoricalChartData;
+      return response.data;
     },
   })
 
   const { data: TrendingCoin } = useQuery({
     queryKey: ['TrendingCoin', currency],
     queryFn: async () => {
-      const marketTrendingCoinData = await fetchMarketTrendingCoinData(currency);
-      setMarketData(marketTrendingCoinData);
+      if (!currency) return null;
+      const response = await fetchMarketTrendingCoinData(currency);
+      setMarketData(response);
       
-      return marketTrendingCoinData;
+      return response.data;
     }
   })
 
