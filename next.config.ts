c8e0e2be7e0a10ad5b7c22771e2c6805f07514b4
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-01 23:52:48 +0900

    fix. 소스맵 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 const nextConfig = {
-  webpack: (config: any) => {
+  webpack: (config: any, { dev }: { dev: boolean }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+    if (dev) {
+      config.devtool = 'source-map';
+    }
+
     return config;
   },
   experimental: {
+    turbopack: true,
     turbo: {
       rules: {
         "*.svg": {
