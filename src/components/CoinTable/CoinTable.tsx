7c34fbbe78629b79fbe1102997d8e0d28bccfa62
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-01 23:53:05 +0900

    design. 코인테이블 스타일링

--- src/components/CoinTable/CoinTable.tsx
+++ src/components/CoinTable/CoinTable.tsx
@@ -18,7 +18,6 @@ const CoinTable = () => {
 
   const data = useMemo(() => coinListAll || [], [coinListAll]);
 
-  // console.log(data, "data")
   const columnHelper = createColumnHelper<any>();
 
   const columns = useMemo(
@@ -28,20 +27,36 @@ const CoinTable = () => {
         cell: (info) => {
           const coin = info.row.original;
           return (
-            <div className='flex items-center'>
+            <div className='flex items-center gap-3'>
               <img
                 src={coin.image}
                 alt={coin.name}
-                className='w-6 h-6'
+                className='w-10 h-10 rounded-full bg-[#edeef0] object-contain'
               />
-              <span>{coin.name}</span>
+              <div>
+                <h3 className='font-medium text-[17px] leading-[25px] text-[#3d414b]'>{coin.name}</h3>
+                <span className='text-[14px] leading-[21px] text-[#acb0b4]'>{coin.symbol.toUpperCase()}</span>
+              </div>
             </div>
           );
         },
       }),
       columnHelper.accessor('current_price', {
         header: '현재가',
-        cell: (info) => `${formatCurrency(info.getValue(), currency)}`,
+        cell: (info) => {
+          const coin = info.row.original;
+          const price = coin.current_price;
+          const priceChange = coin.price_change_24h;
+
+          const isNegative = priceChange < 0;   
+          const textColor = isNegative ? 'text-blue-500' : 'text-red-500';       
+
+          return (
+            <div className={`text-[17px] ${textColor}`}>
+              {formatCurrency(price, currency)}
+            </div>
+          )
+        },
       }),
       columnHelper.accessor(row => ({
         change: row.price_change_24h,
@@ -52,17 +67,29 @@ const CoinTable = () => {
         cell: (info) => {
           const { change, percentage } = info.getValue();
           const formattedChange = change < 1000 ? change.toFixed(4) : Math.floor(change);
+          const isNegative = change < 0;
+          const sign = isNegative ? '' : '+';
+          const textColor = isNegative ? 'text-blue-500' : 'text-red-500';
 
-          return `${formatCurrency(Number(formattedChange), currency)} (${percentage.toFixed(2)}%)`;
+          return (
+            <div className={`relative mr-4 text-[17px] ${textColor}`}>
+              {sign}{formatCurrency(Number(formattedChange), currency)} ({sign}{percentage.toFixed(2)}%)
+              <span
+                className={`absolute right-[-14px] top-0 transform -translate-y-1/2 
+                ${isNegative ? 'after:border-b-[#4386f9]' : 'after:border-b-[#f75457]'} 
+                after:content-[''] after:border-r-[5px] after:border-l-[5px] after:border-transparent after:border-b-[7px]`}
+              />
+            </div>
+          )
         },
       }),
       columnHelper.accessor('total_volume', {
         header: '거래 금액(24H)',
-        cell: (info) => `≈ ${formatCurrency(info.getValue(), currency)}`,
+        cell: (info) => <div className='text-[#3d414b] text-[17px]'>≈ {formatCurrency(info.getValue(), currency)}</div>,
       }),
       columnHelper.accessor('market_cap', {
         header: '시가총액',
-        cell: (info) => formatMarketCapPrice(Number(info.getValue()), currency),
+        cell: (info) => <div className='text-[#3d414b] text-[17px]'>{formatMarketCapPrice(Number(info.getValue()), currency)}</div>,
       }),
     ],
     [currency]
@@ -79,18 +106,34 @@ const CoinTable = () => {
     onSortingChange: setSorting,
   });
 
+  const getColumnAlignmentClass = (index: number) => {
+    if (index === 0) {
+      return 'justify-start';
+    } else if (index === 1) {
+      return 'justify-center';
+    } else {
+      return 'justify-end'; 
+    }
+  };
+
   return (
     <table>
       <thead>
         {table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
+            {headerGroup.headers.map((header, index) => {
+              const isFirst = index === 0;
+              const isLast = index === headerGroup.headers.length - 1;
+        
+              return (
               <th
                 key={header.id}
                 onClick={header.column.getToggleSortingHandler()}
-                className='cursor-pointer'
+                className={`cursor-pointer pt-4 pb-4 bg-[#f8f9fa]
+                          ${isFirst ? 'first:rounded-l-[10px] first:pl-5' : ''} 
+                          ${isLast ? 'last:rounded-r-[10px] last:pr-5' : ''}`}
               >
-                <div className='flex items-center gap-2 text-gray-color'>
+                <div className={`flex gap-2 items-center text-gray-color text-sm leading-[22px] font-normal ${getColumnAlignmentClass(index)}`}>
                   {flexRender(header.column.columnDef.header, header.getContext())}
                   <div className='flex flex-col gap-0.5'>
                     <button>
@@ -104,15 +147,20 @@ const CoinTable = () => {
                   </div>
                 </div>
               </th>
-            ))}
+              )  
+            })}
           </tr>
         ))}
       </thead>
       <tbody>
         {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+          <tr key={row.id} className='gap-0.5'>
+            {row.getVisibleCells().map((cell, index) => (
+              <td key={cell.id}>
+                <div className={`flex h-[88px] items-center ${getColumnAlignmentClass(index)}`}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </div>
+              </td>
             ))}
           </tr>
         ))}
