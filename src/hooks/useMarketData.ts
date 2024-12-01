fe568451aa8f1f0fa04b3723550986694f7f4e27
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-01 21:24:57 +0900

    feat. 코인목록조회 429 error 예외처리

--- src/hooks/useMarketData.ts
+++ src/hooks/useMarketData.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchMarketSingleCoinData, fetchMarketCoinListAllData, fetchMarketHistoricalChartData, fetchMarketTrendingCoinData } from '@/app/api/fetchMarketData';
 import { useMarketStore } from '@/stores/useMarketStore';
 import { Market } from "@/types";
+import { coinListAllMockData } from '@/data/coinListAllMockData';
 
 export const useMarketData = ({id, currency, days}: Market) => {
   const { setMarketData } = useMarketStore();
@@ -21,10 +22,24 @@ export const useMarketData = ({id, currency, days}: Market) => {
     queryKey: ['coinListAll', currency],
     queryFn: async () => {
       if (!currency) return null;
-      const response = await fetchMarketCoinListAllData(currency);
-      return response.data;
+      try {
+        const response = await fetchMarketCoinListAllData(currency);
+        return response.data;
+      } catch (error: any) {
+        if (error.response?.status === 429) {
+          console.warn('API 429 Error(Too many Requests)로 인해 일시적으로 mock data기 사용됩니다.');
+          return coinListAllMockData;
+        }
+
+        if (error.response?.status === 500) {
+          console.error(error.message);
+          return null;
+        }
+      }
     },
-  })
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10 * 60 * 1000,
+  });
 
   const { data: HistoricalChart } = useQuery({
     queryKey: ['HistoricalChart', currency],
