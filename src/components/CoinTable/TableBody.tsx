4e5b8549efa4b9b5e83c6ea1c8197f7cb0c1b224
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-02 21:53:42 +0900

    design. table cell hover color, rounded style

--- src/components/CoinTable/TableBody.tsx
+++ src/components/CoinTable/TableBody.tsx
@@ -1,4 +1,5 @@
 import { flexRender } from '@tanstack/react-table';
+import Link from "next/link";
 
 interface TableBodyProps {
   rows: any[];
@@ -15,15 +16,27 @@ const TableBody = ({ rows }: TableBodyProps) => {
     }
   };
 
+  const getColumnChildrenRoundedClass = (index: number) => {
+    if (index === 0) {
+      return 'rounded-tl-[8px] rounded-bl-[8px]'
+    } else if (index === 4) {
+      return 'rounded-tr-[8px] rounded-br-[8px]'
+    } else {
+      return '';
+    }
+  }
+
   return (
     <>
       {rows.map((row) => (
-        <tr key={row.id}>
+        <tr key={row.id} className="group hover:bg-[#f6f7f8]">
           {row.getVisibleCells().map((cell: any, index: number) => (
-            <td key={cell.id}>
-              <div className={`flex h-[88px] items-center ${getColumnAlignmentClass(index)}`}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </div>
+            <td key={cell.id} className={`px-4 py-2 mt-2 ${getColumnChildrenRoundedClass(index)}`}>
+              <Link href={`${cell.row.original.id}`}>
+                <div className={`flex h-[88px] items-center  ${getColumnAlignmentClass(index)}`}>  
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </div>
+              </Link>
             </td>
           ))}
         </tr>
