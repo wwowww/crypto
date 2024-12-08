d786fd3ceaa7e0cfcbae52f2b36cb80ee558e645
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-08 12:52:00 +0900

    feat. user, like 스키마 정의 및 테이블 생성

---
+++ drizzle/0000_plain_vanisher.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS "likes" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"userId" uuid NOT NULL,
+	"coinSymbol" varchar(50) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "likes" ADD CONSTRAINT "likes_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,29 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, timestamp, uuid, text, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const user = pgTable('users', {
+  id: uuid('id').defaultRandom().notNull().primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const userRelations = relations(user, ({ many }) => ({
+  likes: many(like),
+}));
+
+export const like = pgTable("likes", {
+  id: uuid('id').defaultRandom().notNull().primaryKey(),
+  userId: uuid('userId').references(() => user.id, {onDelete: 'cascade'}).notNull(),
+  coinSymbol: varchar("coinSymbol", { length: 50 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const likeRelations = relations(like, ({ one, many }) => ({
+  user: one(user, {
+    fields: [like.userId],
+    references: [user.id]
+  }),
+}));
