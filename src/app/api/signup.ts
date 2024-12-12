84fbd48276e7ca05c806f9985e1f44f64ea15386
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-12 21:50:22 +0900

    feat. 유효성 검사, 비밀번호 암호화 처리, 이미 존재하는 사용자인지 체크, 폼 데이터 필드 값 추가

---
+++ src/app/api/signup.ts
@@ -0,0 +1,44 @@
+import bcrypt from 'bcryptjs';
+import { db } from "@/db";
+import { user } from "@/db/schema";
+import { SignUpSchema } from "@/schemas/auth";
+import { redirect } from "next/navigation";
+import { getUserByEmail } from '@/data/user';
+
+export const signUp = async (_: any, formData: FormData) => {
+  const validatedFields = SignUpSchema.safeParse({ // safeParse를 사용해 폼 데이터에 있는 필드 값을 넣어줌
+    name: formData.get('name'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  if(!validatedFields.success) {
+    return {
+      errorMessage: "잘못 입력된 값이 있습니다."
+    }
+  }
+
+  const { name, email, password } = validatedFields.data;
+  console.log(name, email, password, "name, email, password")
+
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return {
+        errorMessage: "이미 존재하는 사용자입니다."
+      }
+    }
+  
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    // 3. insert db
+    await db.insert(user).values({ name, email, password: hashedPassword })
+
+  } catch(error) {
+    console.error(error, "error");
+    return { errorMessage: "문제가 발생했습니다." }
+  }
+
+  redirect("/login");
+}
