fc02254ba85854c4afd6a6e214f2ac6410daab9d
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-13 14:15:44 +0900

    feat. 로그인 상태일 때, 로그인 버튼 로그아웃으로 보이도록 설정 및 로그아웃 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,9 +11,19 @@ import {
 } from "@/components/ui/select"
 import Link from "next/link";
 import { useCurrencyStore } from "@/stores/useCurrencyStore";
+import { useUserStore } from "@/stores/useUserStore";
+import { deleteSession } from "@/actions/sessions";
 
 const Header = () => {
   const { currency, setCurrency } = useCurrencyStore();
+  const user = useUserStore((state) => state.user);
+  const updateUser = useUserStore((state) => state.updateUser);
+
+  const handleLogout = async () => {
+    await deleteSession();
+
+    updateUser(null);
+  };
   
   return (
     <div className="flex items-center gap-5 h-[64px] pr-8 pl-7 border-b border-[#e5e7eb]">
@@ -24,12 +34,21 @@ const Header = () => {
         </Link>
       </h1>
       <nav className="w-full flex justify-between items-center">
-        <ul className="flex gap-2">
-          <li>거래소</li>
-          <li>시장동향</li>
-          <li>공지사항</li>
-          <li>로그인</li>
-        </ul>
+        <div className="flex gap-2">
+          <Link href="/btc">
+            <span className="text-4 font-medium">거래소</span>
+          </Link>
+          {!user ? (
+            <Link href="/login">
+              <span className="text-4 font-medium">로그인</span>
+            </Link>
+          ) : (
+            <a className="text-4 font-medium" onClick={() => handleLogout()}>
+              로그아웃
+            </a>
+          )}
+          
+        </div>
         <Select 
           value={currency}
           onValueChange={(value: any) => setCurrency(value)}
