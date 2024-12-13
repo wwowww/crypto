b91673cd635d02bb65a54040b448c1c5dd5914f8
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-13 14:13:27 +0900

    feat. 로그인 상태일 때, 로그인, 회원가입 페이지 접근 안되도록 redirect 설정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,18 @@
-import { ReactNode } from 'react';
+'use client';
+
+import { useUserStore } from '@/stores/useUserStore';
+import { redirect } from "next/navigation"
+import { ReactNode, useEffect } from 'react';
 
 const AuthLayout = ({children}: {children: ReactNode}) => {
+  const user = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    if (user.id) {
+      redirect("/");
+    }
+  }, [user]);
+
   return (
     <div className="flex items-center justify-center h-full">{children}</div>
   );
