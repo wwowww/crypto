d6ab731ae23dc6b942c6659d7d02f803c011e250
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-14 10:16:57 +0900

    feat. 테이블 첫번째 row에 link 삭제

--- src/components/CoinTable/TableBody.tsx
+++ src/components/CoinTable/TableBody.tsx
@@ -15,31 +15,39 @@ const TableBody = ({ rows, isNav=false }: TableBodyProps) => {
     } else if (index === 1 && !isNav) {
       return 'justify-center';
     } else {
-      return 'justify-end'; 
+      return 'justify-end';
     }
   };
 
   const getColumnChildrenRoundedClass = (index: number) => {
     if (index === 0) {
-      return 'rounded-tl-[8px] rounded-bl-[8px]'
+      return 'rounded-tl-[8px] rounded-bl-[8px]';
     } else if (index === 4) {
-      return 'rounded-tr-[8px] rounded-br-[8px]'
+      return 'rounded-tr-[8px] rounded-br-[8px]';
     } else {
       return '';
     }
-  }
+  };
 
   return rows.length !== 0 ? (
-    rows.map((row) => (
+    rows.map((row, rowIndex) => (
       <tr key={row.id} className="group hover:bg-[#f6f7f8]">
-        {row.getVisibleCells().map((cell: any, index: number) => (
-          <td key={cell.id} 
-              className={`${isNav ? 'px-0 py-0 leading-[14px] border-b border-[#eee]' : 'px-4 py-2'} ${getColumnChildrenRoundedClass(index)}`}>
-            <Link href={`${cell.row.original.symbol}`}>
-              <div className={`flex items-center ${getColumnAlignmentClass(index)} ${isNav ? 'h-[50px]' : 'h-[88px]'}`}>
+        {row.getVisibleCells().map((cell: any, cellIndex: number) => (
+          <td
+            key={cell.id}
+            className={`${isNav ? 'px-0 py-0 leading-[14px] border-b border-[#eee]' : 'px-4 py-2'} ${getColumnChildrenRoundedClass(cellIndex)}`}
+          >
+            {cellIndex === 0 ? (
+              <div className={`flex items-center ${getColumnAlignmentClass(cellIndex)} ${isNav ? 'h-[50px]' : 'h-[88px]'}`}>
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </div>
-            </Link>
+            ) : (
+              <Link href={`${cell.row.original.symbol}`}>
+                <div className={`flex items-center ${getColumnAlignmentClass(cellIndex)} ${isNav ? 'h-[50px]' : 'h-[88px]'}`}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </div>
+              </Link>
+            )}
           </td>
         ))}
       </tr>
@@ -47,12 +55,12 @@ const TableBody = ({ rows, isNav=false }: TableBodyProps) => {
   ) : (
     range(0, 10)?.map((item: number) => (
       <tr key={"key"+item}>
-        <td className={`flex items-center space-x-4 h-[105px] ${isNav ? 'h-10 px-2 py-0' : 'px-4 py-2'}`} >
+        <td className={`flex items-center space-x-4 h-[105px] ${isNav ? 'h-10 px-2 py-0' : 'px-4 py-2'}`}>
           {isNav ? '' : <Skeleton className="h-10 w-10 rounded-full" />}
-          <div className={isNav ?'space-y-1' : 'space-y-2'}>
+          <div className={isNav ? 'space-y-1' : 'space-y-2'}>
             <Skeleton className={isNav ? 'h-2 w-[200px]' : 'h-4 w-[450px]'} />
             <Skeleton className={isNav ? 'h-2 w-[150px]' : 'h-4 w-[400px]'} />
-          </div>   
+          </div>
         </td>
       </tr>
     ))
