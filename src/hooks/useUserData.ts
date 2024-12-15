a1ac8c099a82d928d7f5cd7612ebd5e6150ec581
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-15 21:08:19 +0900

    feat. user 상태값 store에 저장, isLoading, isError 추가

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,18 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
+import { useUserStore } from '@/stores/useUserStore';
 import { getUserInfo } from '@/data/user';
 
 export const useUserData = (userId: string | null) => {
-  const { data } = useQuery({
+  const { updateUser } = useUserStore();
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['user', userId],
     queryFn: async () => {
       if (!userId) return null;
       const data = await getUserInfo(userId);
-      
       return data;
-    }
-  })
+    },
+    onSuccess: (data: any) => {
+      if (data) {
+        updateUser(data);
+      }
+    },
+  });
 
-  return { data };
+  return { data, isLoading, isError };
 };
 
 export default useUserData;
