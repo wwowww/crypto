5b39912941b334e67b500d0003d32419364ea701
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-15 16:49:24 +0900

    design. mypage 스타일 추가

--- src/app/mypage/[id]/page.tsx
+++ src/app/mypage/[id]/page.tsx
@@ -5,10 +5,16 @@ import AccountTable from "@/components/UserInfo/AccountTable";
 const MyPage = ({params}: { params: { id: string } }) => {
   return (
     <>
-      
-      <AccountTable />
-      <LogoutModalButton />
-      <DeleteAccountButton />
+      <div>
+        <h3 className="mb-[38px] text-[24px] font-medium tracking-normal text-[#1b1b1b]">
+          회원정보
+        </h3>
+        <AccountTable />
+      </div>
+      <div className="flex justify-end gap-3 pt-[20px] mb-[20px]">
+        <LogoutModalButton className="py-2 px-4 text-white font-semibold bg-[#1b2b3b] hover:bg-[#e15241] transition duration-200 ease rounded" />
+        <DeleteAccountButton className="py-2 px-4 text-white font-semibold bg-[#5d6570] hover:bg-[#e15241] transition duration-200 ease rounded" />
+      </div>
     </>
   )
 }
