b5a26744979375336eb22f97ce199f670d961b3e
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-15 15:53:46 +0900

    feat. 좋아요 버튼 유저 정보 없을 때 안보이도록 예외처리

--- src/components/Button/LikeButton.tsx
+++ src/components/Button/LikeButton.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { addLike, removeLike, checkLike } from "@/actions/likes"; // 서버 사이드 함수들
 import HeartSVG from '@/components/svg/HeartSVG/HeartSVG';
 import { debounce } from 'lodash';
+import { useUserStore } from '@/stores/useUserStore';
+import useUserData from '@/hooks/useUserData';
 
 interface LikeButtonProps {
   coinSymbol: string;
@@ -11,16 +13,23 @@ const LikeButton = ({ coinSymbol }: LikeButtonProps) => {
   const [isLiked, setIsLiked] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
 
+  const user = useUserStore((state) => state.user);
+
+  const userId = user?.id ?? null;
+  const { data } = useUserData(userId);
+  
   const checkIfLiked = debounce(async (symbol: string) => {
     const liked = await checkLike(symbol);
-    setIsLiked(liked);
+    liked !== null && setIsLiked(liked);
   }, 500);
 
   useEffect(() => {
     if (coinSymbol) {
       checkIfLiked(coinSymbol);
     }
-  }, [coinSymbol]);
+  }, [coinSymbol, checkIfLiked]);
+
+  if (data === null || user === null) return null;
 
   const handleLikeToggle = async () => {
     if (isUpdating) return;
@@ -36,17 +45,14 @@ const LikeButton = ({ coinSymbol }: LikeButtonProps) => {
       }
     } catch (error) {
       setIsLiked(isLiked);
-      console.error(error, "LikeButton 업데이트에 실패했습니다.")
+      console.error(error, "LikeButton 업데이트에 실패했습니다.");
     } finally {
       setIsUpdating(false);
     }
   };
 
   return (
-    <button
-      onClick={handleLikeToggle}
-      disabled={isUpdating}
-    >
+    <button onClick={handleLikeToggle} disabled={isUpdating}>
       <HeartSVG color={isLiked ? '#EF4444' : '#ACB0B4'} />
     </button>
   );
