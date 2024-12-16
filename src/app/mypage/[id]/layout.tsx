d94b6bfbfc2cc2555ca195a30bfb268b4cb8127e
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-16 20:43:02 +0900

    feat. mypage use 값 없을 떄 redirect 추가

--- src/app/mypage/[id]/layout.tsx
+++ src/app/mypage/[id]/layout.tsx
@@ -1,8 +1,18 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { useUserStore } from '@/stores/useUserStore';
+import { redirect } from 'next/navigation';
+import { ReactNode, useEffect } from 'react';
 
 const RootLayout = ({children}: {children: ReactNode}) => {
+  const user = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    if (!user || user?.id === '') {
+      redirect("/");
+    }
+  }, [user]);
+  
   return (
     <div className="h-full p-10">{children}</div>
   );
