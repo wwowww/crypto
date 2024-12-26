d60ad8d8c7f8a3ad769b5bff14c7c82faae2cc21
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-26 17:26:32 +0900

    docs. 관심 자산(좋아요 리스트) 추가

--- src/app/mypage/[id]/page.tsx
+++ src/app/mypage/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import LogoutModalButton from "@/components/auth/LogoutModalButton";
 import DeleteAccountButton from "@/components/Button/DeleteAccountButton";
+import LikedCoinsTable from "@/components/CoinTable/LikedCoinsTable";
 import AccountTable from "@/components/UserInfo/AccountTable";
 
 const MyPage = () => {
@@ -13,9 +14,16 @@ const MyPage = () => {
         </h3>
         <AccountTable />
       </div>
-      <div className="flex justify-end gap-3 pt-[20px] mb-[20px]">
+      <div className="flex justify-end gap-3 pt-[20px] mb-[120px]">
         <LogoutModalButton className="py-2 px-4 text-white font-semibold bg-[#1b2b3b] hover:bg-[#e15241] transition duration-200 ease rounded" />
         <DeleteAccountButton className="py-2 px-4 text-white font-semibold bg-[#5d6570] hover:bg-[#e15241] transition duration-200 ease rounded" />
+      </div>
+
+      <div>
+        <h3 className="mb-[38px] text-[24px] font-medium tracking-normal text-[#1b1b1b]">
+          관심 자산
+        </h3>
+        <LikedCoinsTable />
       </div>
     </>
   )
