e0e00574d94df3bcfb50d59275bd54d2bfb47616
wwowww <105021733+wwowww@users.noreply.github.com>
2024-12-28 19:15:50 +0900

    feat. 마이페이지 메타데이터 추가

--- src/app/mypage/[id]/layout.tsx
+++ src/app/mypage/[id]/layout.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import { useUserStore } from '@/stores/useUserStore';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import { ReactNode, useEffect } from 'react';
+
+export const metadata: Metadata = {
+  title: "마이페이지 | Coin Market",
+  description: "Coin Market에 오신 것을 환영합니다. 내 정보와 관심 코인을 확인해보세요.",
+};
+
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   const user = useUserStore((state) => state.user);
